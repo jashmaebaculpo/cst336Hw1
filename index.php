ec57b8ab649a27fd3be68c85d31023305cf7be14
Jashmae Baculpo <jbaculpo@csumb.edu>
2018-02-08 06:04:38 +0000

    add all files

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset=”utf-8” />
+
+        <title> 
+            Progamming Languages
+        </title>
+        
+        <style>
+            body
+            {
+                background-color: #900C3F;
+            }
+        </style>
+        <link href = "styles/styles.css" rel = "stylesheet" type = "text/css" />
+        
+    </head>
+       
+       
+    <body>
+        
+        <header>
+            <h1>
+                Progamming Languages
+            </h1>
+        </header>
+        
+        <nav>
+            <hr width = "50%"/>
+            <a href = "index.php"> Home</a>
+            <a href = "BE.html"> Back-End</a>
+            <a href = "FE.html"> Front-End</a>
+            <a href = "RL.html"> Random Languages</a>
+        </nav>
+        
+        <main>
+            <div id = "welcomeText" >
+                <p>
+                    <big>
+                        In order for programmers to make youur favorite applications or games,
+                        there has to be a way to create those. It generally is made up of instructions that
+                        is read and followed by a computer.
+                        <a href="https://www.computerhope.com/jargon/p/proglang.htm"></a>
+                        
+                        There are many different types and that can get confusing quickly for the 
+                        beginning programmer. But there are normally two general umbrella names for 
+                        languages.
+                    
+                         The two umbrellas are called: 
+                        <p>
+                            <strong>
+                                Back End
+                            </strong>
+                            and 
+                            <strong>
+                                Front End
+                            </strong>
+                            .
+                        </p>
+                        There are more languages than the ones on here, feel free to look for others and practice them!
+                    </big>
+                </p>
+                  
+                <figure id = "confusing"> 
+                    <img src = "https://cdn0.iconfinder.com/data/icons/smiley-emoticons-handdrawn/100/11-512.png"/>
+                    <a href="https://www.iconfinder.com/icons/300033/asking_confused_emoji_emoticon_question_questioning_smiley_icon#size=256"></a>
+                </figure> 
+                
+                
+            </div>
+        
+        <footer>
+            <hr>
+            
+            <p>
+                References used for this page of the website:
+            </p>
+            1.
+            <a href="https://www.computerhope.com/jargon/p/proglang.htm"> Computerhope.com</a>
+            
+            <p>
+                2. 
+            <a href="https://www.iconfinder.com/icons/300033/asking_confused_emoji_emoticon_question_questioning_smiley_icon#size=256"> iconfinder.com</a
+            </p>
+            <p>
+                <img src="/Portfolio-lab/img/CSUMB-logo.png"></img>
+            </p>
+            <strong> Disclaimer: </strong>
+            This is used for academic purposes only.
+            
+            <p>
+            CST 338: Internet Programming. Spring 2018. Baculpo.
+            </p>
+
+        </footer>
+        
+    </body>
+    <!-- closing body -->
+
+</html>
